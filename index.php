4429e9d1879943bc6f0f7f6be44b8fd572ebf679
Peter Benko <peter.benko@gmail.com>
2016-03-04 01:01:50 +0100

    Openings are clickable, change color and remember status.

--- index.php
+++ index.php
@@ -12,39 +12,48 @@
 
     <script type="text/javascript"></script>
 
-    <div id="content"></div>
-    <input type="range" min="0" max="10" step="0.1" id="openingSize" value="2"
-           oninput="showValue(this.id, 'Opening indicator size', this.value)"/>
-    <span id="openingSizeSpan">Opening indicator size: 2</span>
+    <div id="content" style="float:left;">
+
+    </div>
+
+    <div id="controls" style="width: 300px; float:left">
+        <label for="openingSize">Opening indicator size: 2</label>
+        <br/>
+        <input type="range" min="0.1" max="10" step="0.1" id="openingSize" value="2"
+               oninput="showValue(this.id, 'Opening indicator size', this.value)"/>
+        <br/>
+    </div>
 
     <script type="text/javascript">
         function showValue(id, text, newValue)
         {
-            document.getElementById(id+"Span").innerHTML = text + ": " + newValue;
+            getLabel(id).innerHTML = text + ": " + newValue;
         }
     </script>
 
-    <script>
+    <script type="text/javascript">
 
         var container;
 
-        var camera, controls, cameraTarget, scene, renderer;
+        var camera, controls, cameraTarget, scene, renderer, raycaster;
 
         var vein;
 
-        var footerHeight = 100;
+        var controlsWidth = 331;
+
         init();
         animate();
 
         function init() {
             container = document.getElementById("content");
+            container.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
-//            container = document.createElement( 'div' );
-//            document.body.appendChild( container );
+            var width = window.innerWidth - controlsWidth;
+            var height = window.innerHeight;
 
-//            <canvas id="glcanvas" width="1280px" height="720px">
+            container.style.width = width + "px";
 
-            camera = new THREE.PerspectiveCamera( 35, window.innerWidth / (window.innerHeight - footerHeight), 1, 15 );
+            camera = new THREE.PerspectiveCamera( 35, width / height, 1, 15 );
             camera.position.set( 3, 2, 3 );
 
             cameraTarget = new THREE.Vector3( 0, 3, 0 );
@@ -56,12 +65,15 @@
             // Lights
             scene.add( new THREE.HemisphereLight( 0xffffff, 0x0f0f1e ) );
 
+            // Raycaster
+            raycaster = new THREE.Raycaster();
+
             // renderer
             renderer = new THREE.WebGLRenderer( { antialias: true } );
             renderer.setClearColor( 0xfdfd96, 1 );
             renderer.setPixelRatio( window.devicePixelRatio );
 //            renderer.setSize( window.innerWidth, window.innerHeight );
-            renderer.setSize( window.innerWidth, window.innerHeight - footerHeight );
+            renderer.setSize( width, height );
 
             controls = new THREE.OrbitControls( camera, renderer.domElement );
             //controls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
@@ -72,15 +84,45 @@
             container.appendChild( renderer.domElement );
 
             window.addEventListener( 'resize', onWindowResize, false );
-
         }
 
         function onWindowResize() {
 
-            camera.aspect = window.innerWidth / (window.innerHeight - footerHeight);
-            camera.updateProjectionMatrix();
-            renderer.setSize( window.innerWidth, window.innerHeight - footerHeight );
+            var width = window.innerWidth - controlsWidth;
+            var height = window.innerHeight;
+            document.getElementById("content").style.width = width + "px";
 
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+            renderer.setSize( width, height);
+        }
+
+        function onDocumentMouseDown( event ) {
+            event.preventDefault();
+
+            var mouse = new THREE.Vector2(
+                (  event.layerX  / renderer.domElement.width ) * 2 - 1,
+                - (  event.layerY  / renderer.domElement.height ) * 2 + 1);
+
+            raycaster.setFromCamera( mouse, camera );
+
+            if ( vein &&
+                 vein.userData.openings &&
+                 vein.userData.openings.length > 0 )
+            {
+                var objectsToCheck = vein.userData.openings;
+
+                var intersects = raycaster.intersectObjects( objectsToCheck );
+
+                if ( intersects.length > 0 ) {
+                    var intersection = intersects[ 0 ],
+                        obj = intersection.object;
+                    console.log("Clicked: ", obj.userData.id);
+                    obj.userData.isOutlet = !obj.userData.isOutlet;
+                    var color = obj.userData.isOutlet ? 0x0000ff : 0x00ff00;
+                    obj.material.color.setHex(color);
+                }
+            }
         }
 
         function animate() {
@@ -99,22 +141,18 @@
 //            camera.lookAt( cameraTarget );
 //
             var indicatorScale = parseFloat(document.getElementById("openingSize").value);
-//            console.log(" " + indicatorScale)
 
-            if ( vein ) {
-                var veinAccessories = vein.children;
-                if ( veinAccessories.length > 0 ) {
-                    var openingIndicators = veinAccessories[0].children;
-                    if ( openingIndicators.length > 0 )
-                        for (var i = 0, len = openingIndicators.length; i < len; i++) {
-                            var indicator = openingIndicators[i]
-                            indicator.scale.set(indicatorScale,indicatorScale,indicatorScale)
-                        }
+            if ( vein &&
+                 vein.userData.openings &&
+                 vein.userData.openings.length > 0) {
+                var openingIndicators = vein.userData.openings;
+                for (var i = 0, len = openingIndicators.length; i < len; i++) {
+                    var indicator = openingIndicators[i];
+                    indicator.scale.set(indicatorScale, indicatorScale, indicatorScale);
                 }
             }
 
             renderer.render( scene, camera );
-
         }
 
         function loadVein() {
@@ -126,8 +164,9 @@
                 vein = new THREE.Mesh( geometry, material );
 //                vein.add( openingsSpheres );
                 var newGeometry = new THREE.Geometry().fromBufferGeometry( geometry );
-                var openings = loadOpenings( newGeometry );
-                vein.add( openings );
+                var openingsObject = loadOpenings( newGeometry );
+                vein.userData.openings = openingsObject.children;
+                vein.add( openingsObject );
 
                 vein.position.set( 0, -0.75, 0 );
                 vein.rotation.set( - Math.PI / 2, 0, 0 );
@@ -135,7 +174,6 @@
 
                 vein.castShadow = true;
                 vein.receiveShadow = true;
-//                console.log( vein );
                 scene.add( vein );
             } );
         }
@@ -147,21 +185,21 @@
 
             // Mark openings
 
-            // set up the sphere vars
-            var segments = 16, rings = 16;
-
-            // create the sphere's material
-            var sphereMaterial =
-                new THREE.MeshLambertMaterial(
-                    {
-                        color: 0x0000FF,
-                        transparent: true,
-                        opacity: 0.7
-                    });
-
             var openingsSpheres = new THREE.Object3D();
 
             for ( var i = 0, len = openings.length; i < len; i++ ) {
+                // set up the sphere vars
+                var segments = 16, rings = 16;
+
+                // create the sphere's material
+                var sphereMaterial =
+                    new THREE.MeshLambertMaterial(
+                        {
+                            color: 0x0000FF,
+                            transparent: true,
+                            opacity: 0.7
+                        });
+
                 var opening = openings[i];
 
                 var radius = opening[1];
@@ -175,13 +213,26 @@
 
                     sphereMaterial );
 
-                sphere.position.set( center.x, center.y, center.z );
+                sphere.userData.id = i;
+                sphere.userData.isOutlet = true;
 
+                sphere.position.set( center.x, center.y, center.z );
                 // add the sphere to the parent object
                 openingsSpheres.add( sphere );
             }
-            console.log(openingsSpheres);
+
+//            console.log(openingsSpheres);
             return openingsSpheres;
+        }
+
+        function getLabel( needle ) {
+            var labels = document.getElementsByTagName("label");
+            for (var i = 0; i < labels.length; i++) {
+                var label = labels[i];
+                if(label.getAttribute("for") == needle) {
+                    return label;
+                }
+            }
         }
 
     </script>
